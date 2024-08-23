3b3e048214edaf1aad0a504d5a6b94838fdbfb4a
Shaan <shanali.ozhorik@Gmail.com>
2024-08-23 07:09:16 +0500

    commited

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const app = express();
+
+const cookieParser = require("cookie-parser");
 const path = require("path");
 
-app.set("view engine", "ejs");
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-const app = express();
+app.set("view engine", "ejs");
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  app.send("Hello!");
+  res.send("Hello,bro!");
 });
 
 app.listen(PORT, () => {
