6f46ece5f6914cbfcff1ddca0dc68f4c64d85a6e
Shaan <shanali.ozhorik@Gmail.com>
2024-08-23 07:01:38 +0500

    Adding gitKeep file

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const path = require("path");
 
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  app.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
