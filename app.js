4dbd4f6a46a29af075379cefbacf43d7e977791d
Shaan <shanali.ozhorik@Gmail.com>
2024-08-23 07:39:54 +0500

    commited

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
-
 const cookieParser = require("cookie-parser");
 const path = require("path");
-
+const db = require("./config/mongoose-connection");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -15,6 +14,10 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Hello,bro!");
 });
+
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
