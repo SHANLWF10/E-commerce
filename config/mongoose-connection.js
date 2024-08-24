093f1ae138e71f79b9c9f42ffaa80aabca62d80e
Shaan <shanali.ozhorik@Gmail.com>
2024-08-24 17:31:50 +0500

    commited config files

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const config = require("config");
 const dbgr = require("debug")("development:mongoose");
 mongoose
-  .connect(`${config.get("MONGODB_URI")}/`)
+  .connect(`${config.get("MONGODB_URI")}/E-commerce`)
   .then(() => {
     dbgr("Connected");
   })
