29ddf7a5693889af2842002a24801adb35de9460
Shaan <shanali.ozhorik@Gmail.com>
2024-08-24 17:44:13 +0500

    committed

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-
+const ownerModel = require("../models/owner-model");
 router.get("/", (req, res) => {
   res.send("Hey!, It's owner");
 });
+
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", (req, res) => {
+    res.send("Working fine");
+  });
+}
 
 module.exports = router;
