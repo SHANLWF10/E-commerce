b7a0c0c8896089afb709529bd33e848697770c2b
Shaan <shanali.ozhorik@Gmail.com>
2024-08-24 17:19:57 +0500

    commited

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-
+const dbgr = require("debug")("development:mongoose");
 mongoose
   .connect("mongodb://127.0.0.1:27017/E-commerce")
   .then(() => {
-    console.log("Connected");
+    dbgr("Connected");
   })
   .catch((err) => {
     console.log(err);
